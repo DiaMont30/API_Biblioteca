0a9ddcef63df98504973f58ac458b5c70a0b110f
th1agOx <thdarochabp@gmail.com>
2026-05-22 18:31:23 -0300

    Estrutura de pastas da api

---
+++ src/test/java/br/com/escola/apibiblioteca/ApibibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.escola.apibiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApibibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
