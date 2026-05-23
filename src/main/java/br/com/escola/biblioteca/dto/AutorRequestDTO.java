0fd09e7bf56f034337244fcc53b0e314e9c9c8be
Gabriel Aguiar <gabriel44015@gmail.com>
2026-05-22 22:10:08 -0300

    Verificação no Autor DTO

--- src/main/java/br/com/escola/biblioteca/dto/AutorRequestDTO.java
+++ src/main/java/br/com/escola/biblioteca/dto/AutorRequestDTO.java
@@ -2,7 +2,13 @@ package br.com.escola.biblioteca.dto;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record AutorRequestDTO(
+		
+    @NotBlank(message = "O nome do autor é obrigatório")
+    @Size(max = 60, message = "Tamanho máximo 60")
     String nome,
     String nacionalidade,
     LocalDate dataNascimento
