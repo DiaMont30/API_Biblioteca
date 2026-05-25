39079530961609cfbd940eb86dfec615ab75de32
Diana Monteiro <dianar.monteiro@gmail.com>
2026-05-25 17:48:32 -0300

    add: rafatorando Respostas Padrão da API

--- src/main/java/br/com/escola/biblioteca/controller/AutorController.java
+++ src/main/java/br/com/escola/biblioteca/controller/AutorController.java
@@ -7,15 +7,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.escola.biblioteca.annotation.ApiRespostasPadrao;
 import br.com.escola.biblioteca.dto.AutorRequestDTO;
 import br.com.escola.biblioteca.dto.AutorResponseDTO;
-import br.com.escola.biblioteca.entity.Autor;
 import br.com.escola.biblioteca.service.AutorService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -27,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/Autor")
+@ApiRespostasPadrao
 public class AutorController {
 
 	@Autowired
@@ -34,84 +32,38 @@ public class AutorController {
 
 	@GetMapping("/todos-os-autores")
 	@Operation(summary = "Lista todos os autores", description = "A resposta lista os dados dos autores: id, nome, nacionalidade, data de nascimento e lista de livros do autor.")
-	@ApiResponses(value = {
-			@ApiResponse(responseCode = "200", description = "Retorna todos os autores", content = {
-					@Content(schema = @Schema(implementation = Autor.class), mediaType = "application/json") }),
-			@ApiResponse(responseCode = "401", description = "Erro de autenticação"),
-			@ApiResponse(responseCode = "403", description = "Não há permissão para acessar o recurso"),
-			@ApiResponse(responseCode = "404", description = "Recurso não encontrado"),
-			@ApiResponse(responseCode = "500", description = "Exceção interna da aplicação") })
-	public ResponseEntity<List<AutorResponseDTO>> getallAutor() {
+	@ApiResponse(responseCode = "200", description = "Retorna todos os autores")
+	public ResponseEntity<List<AutorResponseDTO>> getAllAutor() {
 		return ResponseEntity.ok(autorService.listarAutores());
 
 	}
 
 	@GetMapping("/{id}")
-    @Operation(
-    	    summary = "Lista um autor de id específico", 
-    	    description = "A resposta lista os dados de um único autor do id informado: id, nome, nacionalidade, data de nascimento e lista de livros do autor."
-    	)
-    	@ApiResponses(value = {
-    	    @ApiResponse(responseCode = "200", description = "Retorna o autor especificado",
-    	        content = {@Content(schema = @Schema(implementation = Autor.class), mediaType = "application/json")}
-    	    ),
-    	    @ApiResponse(responseCode = "401", description = "Erro de autenticação"),
-    	    @ApiResponse(responseCode = "403", description = "Não há permissão para acessar o recurso"),
-    	    @ApiResponse(responseCode = "404", description = "Recurso não encontrado"),
-    	    @ApiResponse(responseCode = "500", description = "Exceção interna da aplicação") 
-    	})
+	@Operation(summary = "Lista um autor de id específico", description = "A resposta lista os dados de um único autor do id informado: id, nome, nacionalidade, data de nascimento e lista de livros do autor.")
+	@ApiResponse(responseCode = "200", description = "Retorna o autor especificado")
 	public ResponseEntity<AutorResponseDTO> getAutorByid(@PathVariable Long id) {
 		return ResponseEntity.ok(autorService.obterAutorPorId(id));
 	}
 
 	@PostMapping("adicionar-autor")
-    @Operation(
-    	    summary = "Cadastrar Autor", 
-    	    description = "Insere um novo autor na base de dados e retorna o objeto criado com seu ID gerado."
-    	)
-    	@ApiResponses(value = {
-    	    @ApiResponse(
-    	        responseCode = "201", 
-    	        description = "Autor criado com sucesso",
-    	        content = { @Content(schema = @Schema(implementation = Autor.class), mediaType = "application/json") }
-    	    ),
-    	    @ApiResponse(responseCode = "400", description = "Dados inválidos enviados no corpo da requisição"),
-    	    @ApiResponse(responseCode = "401", description = "Erro de autenticação"),
-    	    @ApiResponse(responseCode = "500", description = "Erro interno ao processar o cadastro")
-    	})
+	@Operation(summary = "Cadastrar Autor", description = "Insere um novo autor na base de dados e retorna o objeto criado com seu ID gerado.")
+	@ApiResponse(responseCode = "201", description = "Autor criado com sucesso")
+	@ApiResponse(responseCode = "400", description = "Dados inválidos enviados no corpo da requisição")
 	public ResponseEntity<AutorResponseDTO> adicionarAutor(@Valid @RequestBody AutorRequestDTO dto) {
 		return ResponseEntity.ok(autorService.salvar(dto));
 	}
 
 	@PutMapping("/atualizar-autor/{id}")
-    @Operation(
-    	    summary = "Atualizar Autor", 
-    	    description = "Atualiza dados de um autor"
-    	)
-    	@ApiResponses(value = {
-    	    @ApiResponse(responseCode = "200", description = "Autor Atualizado"),
-    	    @ApiResponse(responseCode = "401", description = "Erro de autenticação"),
-    	    @ApiResponse(responseCode = "403", description = "Não há permissão para acessar o recurso"),
-    	    @ApiResponse(responseCode = "404", description = "Recurso não encontrado"),
-    	    @ApiResponse(responseCode = "500", description = "Exceção interna da aplicação")
-    	})
+	@Operation(summary = "Atualizar Autor", description = "Atualiza dados de um autor")
+	@ApiResponse(responseCode = "200", description = "Autor Atualizado")
 	public ResponseEntity<AutorResponseDTO> atualizarAutor(@PathVariable Long id,
 			@Valid @RequestBody AutorRequestDTO dto) {
 		return ResponseEntity.ok(autorService.atualizar(id, dto));
 	}
 
 	@DeleteMapping("/deletar-autor/{id}")
-	 @Operation(
-	    	    summary = "Remover Autor", 
-	    	    description = "Remove um autor permanentemente da base de dados"
-	    	)
-	    	@ApiResponses(value = {
-	    	    @ApiResponse(responseCode = "204", description = "Autor Removido com Sucesso (Sem conteúdo de retorno)"),
-	    	    @ApiResponse(responseCode = "401", description = "Erro de autenticação"),
-	    	    @ApiResponse(responseCode = "403", description = "Não há permissão para acessar o recurso"),
-	    	    @ApiResponse(responseCode = "404", description = "Recurso não encontrado"),
-	    	    @ApiResponse(responseCode = "500", description = "Exceção interna da aplicação")
-	    	})
+	@Operation(summary = "Remover Autor", description = "Remove um autor permanentemente da base de dados")
+	@ApiResponse(responseCode = "204", description = "Autor Removido com Sucesso (Sem conteúdo de retorno)")
 	public ResponseEntity<Void> deletarAutor(@PathVariable Long id) {
 		autorService.deletar(id);
 		return ResponseEntity.noContent().build();
