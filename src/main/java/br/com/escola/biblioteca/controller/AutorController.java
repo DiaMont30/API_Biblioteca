4671b475b5583f2b6ad51a8ea1615ef73db7a37a
Gabriel Aguiar <gabriel44015@gmail.com>
2026-05-25 17:38:53 -0300

    AJustes to Swagger e descrições detalhadas

--- src/main/java/br/com/escola/biblioteca/controller/AutorController.java
+++ src/main/java/br/com/escola/biblioteca/controller/AutorController.java
@@ -4,6 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,17 +22,12 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/Autor")
+@Tag(name = "Autor", description = "Endpoints para gerenciamento de autor")
 public class AutorController {
 
 	@Autowired
--- src/main/java/br/com/escola/biblioteca/dto/AutorResponseDTO.java
+++ src/main/java/br/com/escola/biblioteca/dto/AutorResponseDTO.java
@@ -4,11 +4,23 @@ import java.time.LocalDate;
 import java.util.List;
 
 import br.com.escola.biblioteca.entity.Livro;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "Objeto de resposta com os dados detalhados do autor")
 public record AutorResponseDTO(
+		
+		@Schema(description="Identificador unico do autor", example="1")
         Long id,
+        
+        @Schema(description = "Nome do autor", example = "João da Silva", requiredMode = Schema.RequiredMode.REQUIRED)
         String nome,
+        
+    	@Schema(description="Nacionalidado do autor", example="Brasileiro")
         String nacionalidade,
+        
+    	@Schema(description="Data de nascimento do autor", example="2000-04-04")
         LocalDate dataNascimento,
+        
+    	@Schema(description="Lista de livros do autor")
         List<Livro> livros) {
 }
